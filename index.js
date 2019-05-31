fa2ad8c43fb8d564e378d2a66f561d9b3d3a42c8
TheAlacrity <brinker.jeff@gmail.com>
2019-05-31 10:20:23 -0500

    added more js packages

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ var moment = require('moment')
 console.log("Hello from Javascript!")
 var datetime = moment().startOf('day').fromNow();
 console.log(datetime);
+
+var geolocation = require('geolocation')
+ 
+geolocation.getCurrentPosition(function (err, position) {
+  if (err) throw err
+  console.log(position)
+})
+
+const pokemon = require('pokemon');
+ 
